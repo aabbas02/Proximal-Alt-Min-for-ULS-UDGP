d8b288548eb027d79d5d64a3bb649ec94d9e1216
Ahmed Abbasi <ahmed.abbasi@tufts.edu>
2022-05-29 13:59:48 -0400

    Update main_3a.m

--- figures/main_3a.m
+++ figures/main_3a.m
@@ -15,10 +15,6 @@ MC              = 5;
 SNR             = 100;
 d               = 100;
 m               = 50;
-%r_              = [10 50 100 125 200];
-%r_              = [20 25 40 50 100 125];
-%r_              = [10 20 25 40 50 100];
-%r_              = [20 25 50 100 125 150];
 r_              = [20 25 40 50 100 125 200];
 n               = 1000;
 d_H_levsort     = zeros(1,length(r_));
@@ -41,13 +37,13 @@ for j = 1 : length(r_)
                 pi_              = make_r_local_permutation(n,r);
                 Y_permuted       = pi_*Y;
                 Y_permuted_noisy = Y_permuted + W;
-				%---rlus  https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=9440727 
+		%---rlus  https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=9440727 
                 %t1_start = tic;
                 [pi_fw]        = rlus(B,Y_permuted_noisy,r);
                 %toc(t1_start)
                 d_H            = sum(sum(pi_ ~= pi_fw))/(2*n);
                 d_H_rlus(j)    = d_H + d_H_rlus(1,j);
-				%---biconvex https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8849447
+		%---biconvex https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8849447
 				d_H_min = 1;
                 for i = 1 : length(rho_) % cross validate across rho paramter
                 %for i = 1 : 1
@@ -59,13 +55,13 @@ for j = 1 : length(r_)
                    end
                 end
                d_H_biconvex(j) = d_H_biconvex(j) + d_H_min;
-                %---icml https://proceedings.mlr.press/v119/zhang20n.html
+               %---icml https://proceedings.mlr.press/v119/zhang20n.html
                pi_icml            = icml_20(B,Y_permuted_noisy,r);
                d_H_one_step(j)    = d_H_one_step(j) + sum(sum(pi_ ~= pi_icml))/(2*n);   
                %---levsort  https://people.eecs.berkeley.edu/~courtade/pdfs/DenoisingLinearModels_ISIT2017.pdf
                pi_lev             = levsort(B,Y_permuted_noisy,r);
                d_H_levsort(j)     = d_H_levsort(j) + sum(sum(pi_ ~= pi_lev))/(2*n);                   
-				%---alt-min/proposed
+	       %---alt-min/proposed
                %t_alt_min = tic;
                [~,pi_lp]          = lp_ls_alt_min_prox(B,Y_permuted_noisy,r,lbd);
                %toc(t_alt_min)
